62773f7c43cfd2dfbd3baf469732e8e329ee650f
gerara11 <gorelikov.german@yandex.ru>
2026-01-05 18:32:45 +0300

    Исправлены опечатки

--- Motion.py
+++ Motion.py
@@ -3,9 +3,9 @@ from cmath import sin
 # законы движения для 2х координат
 def MotionLaw1(t,x):
     velo1 = -t * x
-    return velo11
+    return velo1
 
 def MotionLaw2(t,x):
     velo2 = -sin(t)*x
-    return velo22
+    return velo2
 
