18087cb14593234ef842b46dcdfaffa434012507
gerara11 <gorelikov.german@yandex.ru>
2026-01-05 21:33:35 +0300

    Исправлен вывод начального положения и добавлен график распределения скоростей

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from Class_Ring import Ring
-from Plots import risunok
+from Plots import risunok, risunok_velo
 from Time import TimeList
 from Trajectory import TrajectoryByX1
 from Trajectory import TrajectoryByX2
@@ -15,8 +15,8 @@ koord1 = np.zeros(len(all_ring))
 koord2 = np.zeros(len(all_ring))
 for i in range(len(all_ring)):
     #Изменить в зависимости от интерпретации координат точки
-    koord1[i] = TrajectoryByX1(all_ring[i].x,t,h)
-    koord2[i] = TrajectoryByX2(all_ring[i].y,t,h)
-risunok(koord1,koord2)
-
+    koord1[i] = TrajectoryByX1(all_ring[0,i],t,h)
+    koord2[i] = TrajectoryByX2(all_ring[1,i],t,h)
+risunok(koord1,koord2,t)
+risunok_velo(koord1,koord2,t)
 
--- Plots.py
+++ Plots.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+from Integrator import RangeKutta
+from Motion import MotionLaw1
+from Motion import MotionLaw2
+
 def risunok(x1, x2, t):
         for i in range(len(t)):
                 plt.plot(x1[i], x2[i], 'r-')
         plt.show()
+def risunok_velo(x1,x2,t):
+        v1 = np.zeros(len(t))
+        v2 = np.zeros(len(t))
+        for i in range(len(t)):
+                v1[i] = MotionLaw1(t[i],x1[i])
+                v2[i] = MotionLaw2(t[i],x2[i])
+        for i in range(len(t)):
+                plt.plot(v1[i], v2[i], 'b-')
+        plt.show()
+
