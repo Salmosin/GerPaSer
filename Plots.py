d8530d46f5bc8ffde1ac744b1ac5e275967829a2
gerara11 <gorelikov.german@yandex.ru>
2026-01-06 13:32:43 +0300

    Добавлен график линий тока

--- Plots.py
+++ Plots.py
@@ -1,3 +1,5 @@
+import cmath
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,5 +19,20 @@ def risunok_velo(x1,x2,t):
                 v2[i] = MotionLaw2(t[i],x2[i])
         for i in range(len(t)):
                 plt.plot(v1[i], v2[i], 'b-')
+        plt.show()
+def risunok_lines(x1,x2,t,x0,h):
+        v1 = np.zeros(len(t))
+        v2 = np.zeros(len(t))
+        sl1 = np.zeros(len(t))
+        sl2 = np.zeros(len(t))
+        for i in range(len(t)):
+                v1[i] = MotionLaw1(t,x1[i])
+                v2[i] = MotionLaw2(t,x2[i])
+                ang = RangeKutta(x0,t,h,MotionLaw1)
+                sl1[i] = v1[i] + 1 / cmath.sqrt(1 + ang ** 2)
+                sl2[i] = v2[i] + ang / cmath.sqrt(1 + ang ** 2)
+        for i in range(len(t)):
+                plt.plot([v1[i], sl1[i]], [v2[i], sl2[i]], 'b-')
+                plt.plot(v1[i], v2[i], 'ro', )
         plt.show()
 
